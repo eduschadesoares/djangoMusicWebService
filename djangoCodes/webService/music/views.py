723685a1b3d4810345be835e30b14898f2e6809f
Eduardo <eduschadesoares@hotmail.com>
2018-06-29 04:04:21 -0300

    Views updated

--- djangoCodes/webService/music/views.py
+++ djangoCodes/webService/music/views.py
@@ -1,11 +1,56 @@
 from django.shortcuts import render
+
 from rest_framework import generics
-from .models import Music
-from .serializers import MusicSerializer
+from .serializers import (MusicSerializer,
+                          BandSerializer,
+                          RecordSerializer,
+                          GenreSerializer,
+                          PlaylistSerializer,
+                          )
+from .models import (Music,
+                     Band,
+                     Record,
+                     Genre,
+                     Playlist,
+                     )
 
 # Create your views here.
 class MusicList(generics.ListCreateAPIView):
-
     queryset = Music.objects.all()
     serializer_class = MusicSerializer
 
+class MusicDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Music.objects.all()
+    serializer_class = MusicSerializer
+
+class BandList(generics.ListCreateAPIView):
+    queryset = Band.objects.all()
+    serializer_class = BandSerializer
+
+class BandDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Band.objects.all()
+    serializer_class = BandSerializer
+
+class RecordList(generics.ListCreateAPIView):
+    queryset = Record.objects.all()
+    serializer_class = RecordSerializer
+
+class RecordDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Record.objects.all()
+    serializer_class = RecordSerializer
+
+class GenreList(generics.ListCreateAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+class GenreDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+class PlaylistList(generics.ListCreateAPIView):
+    queryset = Playlist.objects.all()
+    serializer_class = PlaylistSerializer
+
+class PlaylistDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Playlist.objects.all()
+    serializer_class = PlaylistSerializer
