396e8b3681f7b108acf8f0232d3cb1dd35242667
Eduardo <eduschadesoares@hotmail.com>
2018-06-29 04:04:59 -0300

    Last updates

--- djangoCodes/webService/webService/urls.py
+++ djangoCodes/webService/webService/urls.py
@@ -18,6 +18,6 @@ router.register(r'groups', views.GroupViewSet)
 urlpatterns = [
     # url(r'^', include(router.urls)),
     # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path(r'', include('music.urls')),
+    path(r'', include('music.urls'), name='music'),
     path('admin/', admin.site.urls),
 ]
