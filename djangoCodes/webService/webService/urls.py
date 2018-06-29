e0daecb8387f6e1ab01d0d1b6b556440f606bee4
Eduardo <eduschadesoares@hotmail.com>
2018-06-29 03:36:47 -0300

    Changed urls

--- djangoCodes/webService/webService/urls.py
+++ djangoCodes/webService/webService/urls.py
@@ -7,14 +7,17 @@ from API import views
 from django.contrib import admin
 from django.urls import include, path
 
-router = routers.DefaultRouter()
+"""router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
+"""
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
+
 urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    # url(r'^', include(router.urls)),
+    # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path(r'', include('music.urls')),
     path('admin/', admin.site.urls),
 ]
