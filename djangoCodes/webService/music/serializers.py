5ffe68e9233a31a55dc7a290cf7a3e0af1b472c3
Eduardo <eduschadesoares@hotmail.com>
2018-06-29 03:46:25 -0300

    Serializers music app

--- djangoCodes/webService/music/serializers.py
+++ djangoCodes/webService/music/serializers.py
@@ -7,7 +7,26 @@ from .models import (Music,
                      )
 
 class MusicSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Music
+        fields = '__all__'
+
+class BandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Band
+        fields = '__all__'
+
+class RecordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Record
+        fields = '__all__'
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = '__all__'
+
+class Playlist(serializers.ModelSerializer):
+    class Meta:
+        model = Playlist
         fields = '__all__'
