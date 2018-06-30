e577ee84ecf6f4fe02d3a18ba5a612d65d74f0d9
Eduardo <eduschadesoares@hotmail.com>
2018-06-29 23:27:46 -0300

    Last updates

--- djangoCodes/webService/music/views.py
+++ djangoCodes/webService/music/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAdminUser, IsAuthenticated, AllowAny
+from django_filters import rest_framework as filters
+from rest_condition import Or
+from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, TokenHasScope, OAuth2Authentication
+from rest_framework.authentication import SessionAuthentication
+
 
 from rest_framework import generics
 from .serializers import (MusicSerializer,
@@ -20,8 +24,11 @@ from .models import (Music,
 class MusicList(generics.ListCreateAPIView):
     queryset = Music.objects.all()
     serializer_class = MusicSerializer
-    authentication_classes = [SessionAuthentication]
-    permission_classes = (IsAuthenticated, )
+    authentication_classes = [OAuth2Authentication, SessionAuthentication]
+    permission_classes = [Or(IsAdminUser, TokenHasReadWriteScope)]
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_fields = '__all__'
+
 
 class MusicDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Music.objects.all()
